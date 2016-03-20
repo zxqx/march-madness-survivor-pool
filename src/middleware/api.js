f41ed7e46b4e09f3706958141757bf464a57f687
Zak Angelle <zaknotzak@gmail.com>
2016-03-20 13:52:00 -0500

    Cleanup

--- src/middleware/api.js
+++ src/middleware/api.js
@@ -3,11 +3,8 @@ import ncaam2016 from 'bracket-data/browser/ncaam-2016';
 export async function fetchTeamList() {
   const { regions } = ncaam2016.bracket;
 
-  const teams = Object.keys(regions)
+  return Object.keys(regions)
     .filter(regionId => regionId !== 'FF')
     .map(regionId => regions[regionId].teams)
     .reduce((a, b) => a.concat(b));
-
-  return teams;
 }
-
