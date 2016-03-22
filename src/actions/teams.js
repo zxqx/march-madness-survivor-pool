cb05660f8b198ac0f9b5d01eed013d375d2412e3
Zak Angelle <zaknotzak@gmail.com>
2016-03-22 11:53:18 -0500

    Added clear team list action

--- src/actions/teams.js
+++ src/actions/teams.js
@@ -1,6 +1,7 @@
 import { fetchTeamList } from '../middleware/api';
 
 export const UPDATE_TEAM_LIST = 'UPDATE_TEAM_LIST';
+export const CLEAR_TEAM_LIST = 'CLEAR_TEAM_LIST';
 
 export function updateTeamList(payload) {
   return {
@@ -13,5 +14,11 @@ export function getTeamList() {
   return async function(dispatch) {
     const payload = await fetchTeamList();
     dispatch(updateTeamList(payload));
+  };
+}
+
+export function clearTeamList() {
+  return {
+    type: CLEAR_TEAM_LIST
   };
 }
--- src/reducers/teams.js
+++ src/reducers/teams.js
@@ -1,10 +1,13 @@
 import { createReducer } from 'redux-create-reducer';
-import { UPDATE_TEAM_LIST } from '../actions/teams';
+import { UPDATE_TEAM_LIST, CLEAR_TEAM_LIST } from '../actions/teams';
 
 const initialState = [];
 
 export default createReducer(initialState, {
   [UPDATE_TEAM_LIST](state, action) {
     return action.payload;
+  },
+  [CLEAR_TEAM_LIST](state, action) {
+    return initialState;
   }
 });
